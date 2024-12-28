0d729c42b6bd684d8031c7c9a43fdc4e2363b57e
TaleEfe <146596534+TaleEfe@users.noreply.github.com>
2024-12-28 02:23:31 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SECRET_KEY = os.urandom(24)
-    CLIENT_KEY = 'm7fTvj1L5YPhBXZvJuYqAQX0zeAhBwMsfF9eYf4nhTcaux3k'
-    CLIENT_SECRET = '7hD4GQOmXxd93hRGIwtqTJC0PPjO3Dn1BarHqMgD2JPe2yUsL2sAZyP3MExgFoYC'
+    CLIENT_KEY = 'Key'
+    CLIENT_SECRET = 'Secret'
     SCOPE = 'umscheduleofclasses'
     SMTP_SERVER = 'smtp.gmail.com'
     SMTP_PORT = 587
     SMTP_USERNAME = 'taleefe@gmail.com'
-    SMTP_PASSWORD = 'wiaaacwgwodsdnem'
+    SMTP_PASSWORD = 'Password'
     FROM_EMAIL = 'taleefe@gmail.com'
